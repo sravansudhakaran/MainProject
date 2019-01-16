2fc7f3d84a38552e0a5c1665bf2e00a73c13e23d
sravansudhakaran <sudhakaransravan@gmail.com>
2019-01-16 12:16:25 +0530

    clustering

---
+++ Clustering.py
@@ -0,0 +1,24 @@
+from numpy import random, array
+
+
+def createCluster(N, K):
+    random.seed(10)
+    pointperCluster = float(N) / K
+    x = []
+    for i in range(K):
+        collectorCurrentCentroid = random.uniform()
+        collectorVoltageCentroid = random.uniform()
+        gateCurrentCentroid = random.uniform()
+        gateVoltageCentroid = random.uniform()
+        packageTemperatureCentroid = random.uniform()
+        for j in range(int(point))
+
+
+def main(args):
+    return 0
+
+
+if __name__ == '__main__':
+    import sys
+
+    sys.exit(main(sys.argv))
