845f17067ee748138238157ccadffb172dd90cfa
mensider <mensider@github.com>
2019-04-08 13:54:36 +0530

    working on Sensor_controller.py

--- BF494/Predict_BF494.py
+++ BF494/Predict_BF494.py
@@ -7,12 +7,20 @@ from keras.models import load_model
 import os
 import Sensor_controller
 import math
+import time
+
 
 def get_params():
 	
-	vce,vbe = Sensor_controller.get_vce_vbe()
-	ambient_temp = Sensor_controller.get_temp()
-	
+	vce,vbe,ambient_temp = Sensor_controller.get_values()
+	os.system('clear')
+	print('[+] Sensor Readings Obtained ...')
+	time.sleep(1)
+	print('[+] Vbe = ' + str(vbe))
+	print('[+] Vce = ' + str(vce))
+	print('[+] Ambient Temperature = ' + str(ambient_temp))	
+	time.sleep(1)
+	print('[+] Calculating Additional Parameters ...')
 	vcc = 12.0
 	vbb = 5.0
 	rc = 100.0
@@ -24,7 +32,8 @@ def get_params():
 	alpha = beta / (1+beta)
 	power_diss = 0.100
 	junction_temp = ambient_temp + ( thermal_res * power_diss )
-	
+	time.sleep(1)
+	print('[+] Additional Parameters Calculated ...')
 	sensor_values = [vce,vbe,ic,ib,beta,alpha,ambient_temp,junction_temp]
 	return sensor_values
 
@@ -32,6 +41,8 @@ def root_mean_squared_error(y_true, y_pred):
 	return backend.sqrt(backend.mean(backend.square(y_pred - y_true)))
 
 def predict_rul():
+	print('[+] Loading Trained Model and predicting RUL ...')
+	time.sleep(1)
 	model = load_model('BF494.hdf5', custom_objects ={'root_mean_squared_error':root_mean_squared_error})
 	X = np.array([get_params()])
 	Y = model.predict(X)
--- BF494/Sensor_controller.py
+++ BF494/Sensor_controller.py
@@ -1,11 +1,23 @@
-import Rpi.GPIO
-import os
-import math
+import RPi.GPIO as IO
 
-def get_vce_vbe():
-	#get vce and vbe from ADC0808
-	return (12,1)
+def get_values():
+	#initial_setup	
+	IO.setwarnings(True)
+	IO.setmode (IO.BCM)
+	vce = 12.0
+	vbe = 1.0
+	ambient_temp = 30.0
 	
-def get_temp():
-	#get temperature from LM35
-	return (30)
+	#8 Digital pins with incoming ADC output
+	IO.setup(4,IO.IN)
+	IO.setup(17,IO.IN)
+	IO.setup(27,IO.IN)
+	IO.setup(22,IO.IN)
+	IO.setup(5,IO.IN)
+	IO.setup(6,IO.IN)
+	IO.setup(13,IO.IN)
+	IO.setup(19,IO.IN)
+	
+	#get vce and vbe and temp from ADC0808
+	
+	return (vce,vbe,ambient_temp)
