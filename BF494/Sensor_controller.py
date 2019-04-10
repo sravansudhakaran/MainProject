c56c838eec0963a79f5b08a76b7804bf23a7cf7d
Your Name <you@example.com>
2019-04-10 20:28:15 +0000

    changed pi pin numbers

--- BF494/Sensor_controller.py
+++ BF494/Sensor_controller.py
@@ -4,14 +4,17 @@ import os
 
 def get_values():
     GPIO.setmode(GPIO.BCM)
-    GPIO.setwarnings(False)
+    GPIO.setwarnings(True)
+    print("1")
     #Digital input pins from ADC0808
-    binarys = (35,29,33,31,40,38,36,32)
+    #binarys = (35,29,33,31,40,38,36,32)
+    binarys = [19,5,13,6,21,20,16,12]
     #Initialy set to zero value
     bits = [0,0,0,0,0,0,0,0]
     for binary in binarys:
         GPIO.setup(binary, GPIO.IN) #All Digital pins are input pins
-
+        print("Pin Set")
+    print("2")
     #Select pin Select Pin LSB
     GPIO.setup(16, GPIO.OUT)
     #Select pin Select Pin MSB
@@ -19,7 +22,7 @@ def get_values():
     decimal_vce = 0
     decimal_vbe = 0
     decimal_temp = 0
-	
+    print("3")
     #Calculating Vce
     GPIO.output(16,GPIO.LOW)
     GPIO.output(18,GPIO.LOW)
@@ -32,7 +35,7 @@ def get_values():
     #Calculating decimal value of input
     for i in range(8):
         decimal_temp = decimal_temp + (bits[i] * (2**(7-i)))
-	
+    print("4")
     #Calculating Vbe
     GPIO.output(16,GPIO.HIGH)
     GPIO.output(18,GPIO.LOW)
@@ -45,7 +48,7 @@ def get_values():
     #Calculating decimal value of input
     for i in range(8):
         decimal_vbe = decimal_vbe + (bits[i] * (2**(7-i)))
-	
+    print("5")
     #Calculating Temperature
     GPIO.output(16,GPIO.LOW)
     GPIO.output(18,GPIO.HIGH)
@@ -58,7 +61,7 @@ def get_values():
     #Calculating decimal value of input
     for i in range(8):
         decimal_vce = decimal_vce + (bits[i] * (2**(7-i)))
-
+    print("6")
 
     return (decimal_vce,decimal_vbe,decimal_temp)
 
--- BF494/clock.py
+++ BF494/clock.py
@@ -3,13 +3,13 @@ import time
 def clock_ale_soc():
 	GPIO.setmode(GPIO.BCM)
 	GPIO.setup(4,GPIO.ALT0)
-	GPIO.setup(12,GPIO.OUT)
+	GPIO.setup(18,GPIO.OUT)
 	GPIO.setclock(4,500000)
 	GPIO.output(4,1)
 	while 1:
-		GPIO.output(12,GPIO.HIGH)
+		GPIO.output(18,GPIO.HIGH)
 		time.sleep(0.3)
-		GPIO.output(12,GPIO.LOW)
+		GPIO.output(18,GPIO.LOW)
 		time.sleep(1)
 	return
 
