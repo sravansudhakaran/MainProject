03308be5bf585e568e7ee58f4386432395216c6c
mensider <mensider@github.com>
2019-06-09 20:58:00 +0530

    MGMOS

--- Final/pi_server.py
+++ Final/pi_server.py
@@ -2,23 +2,25 @@ import time
 import serial
 
 def get_data():
-	no_samples = 10
+	no_samples = 20
 	vce_array = []
 	vbe_array = []
 	temp_array = []
 	ser = serial.Serial('/dev/ttyACM0', 9600)         			# enable the serial port
-	for i in range(0,20):                                                   # execute the loop forever
+	for i in range(0,50):                                                   # execute the loop forever
 		incoming_stream = ser.readline()                                # read the serial data sent by the UNO
-		#print (incoming_stream)                                         # print the serial data sent by UNO
+		print (incoming_stream)                                         # print the serial data sent by UNO
 		try:
 			values = incoming_stream.decode("utf-8")		# decode bytes to string
 			(temp,vbe,vce) = values.split(',')
 			if(len(temp_array) < no_samples):
 				temp_array.append(float(temp))
 			if(len(vbe_array) < no_samples):
-				vbe_array.append(float(vbe))
+				if(float(vbe) > 0.9):
+					vbe_array.append(float(vbe))
 			if(len(vce_array) < no_samples):
-				vce_array.append(float(vce))
+				if(float(vce) < 1.0):
+					vce_array.append(float(vce))
 
 		except:
 			continue
